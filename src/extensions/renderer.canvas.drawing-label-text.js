fb1d495b4829eb78dc2802f5763811aa0fa321c1
Rui Cheng <rui.cheng@infotrack.com.au>
2015-02-23 17:40:25 +1100

    refined the wrapText testWidth based on font size

--- src/extensions/renderer.canvas.drawing-label-text.js
+++ src/extensions/renderer.canvas.drawing-label-text.js
@@ -189,6 +189,7 @@
       var testLine = line + words[n] + ' ';
       var metrics = context.measureText(testLine);
       var testWidth = metrics.width;
+      testWidth = testWidth / (window.reveal.fontSize / 7);
       if (testWidth > maxWidth && n > 0) {
         context.fillText(line, x, y);
         line = words[n] + ' ';
